ce32304e266e63ae6b0cc6d2118a4d1106370a27
MyroslavKl <muroslavkl@gmail.com>
2023-03-10 13:14:08 +0200

    revert added description

--- ConsoleWords/Models/Language.cs
+++ ConsoleWords/Models/Language.cs
@@ -11,17 +11,12 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
-        public String Description { get; set; }
-        public Language(Guid id, string title, List<Word> words, string description);
-
-        public Language(Guid id, string title, List<Word> words, string description, List<Group> groups)
+        public Language(Guid id, string title, List<Word> words)
         {
             Id = id;
             Title = title;
             Words = words;
-			Description = description;
-        }
-
+            }
         public double getAverageLevel()
         {
             return Words.Sum(x => x.Level) / Words.Count;
